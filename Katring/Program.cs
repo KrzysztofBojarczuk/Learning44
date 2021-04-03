a5b6dc74977b0a068855f54800f698030accdae4
KrzysztofBojarczuk <69596796+KrzysztofBojarczuk@users.noreply.github.com>
2021-04-03 09:25:14 +0200

    Add project files.

---
+++ Katring/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Katring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            showIntro();
+            Console.WriteLine();
+            Console.WriteLine("Wprowadź liczbę szklanek składnika.");
+            int cupsNeeded = int.Parse(Console.ReadLine());
+            cupsToOunces(cupsNeeded);
+        }
+        public static void showIntro()
+        {
+            Console.WriteLine("Program służy do zmiany szkalnek");
+            Console.WriteLine("na uncję. Dla Twojej wiadomości");
+            Console.WriteLine("  1 szklanka = 8 uncji.");
+        }
+        public static void cupsToOunces(int cups)
+        {
+            int ounces;
+            ounces = cups * 8;
+
+            Console.WriteLine($"Ta ilość odpowaida {ounces} uncjom");
+        }
+    }
+}
